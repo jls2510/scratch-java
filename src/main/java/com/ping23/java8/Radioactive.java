71e54a35f39d6137a97c468c3faa9eaaf8760bd9
James L. Stevenson <jim@ping23.com>
2019-12-20 10:43:10 -0500

    Removed encryption package. See scratch-java-crypt for better stuff.

---
+++ src/main/java/com/ping23/java8/Radioactive.java
@@ -0,0 +1,34 @@
+package com.ping23.java8;
+
+import java.util.function.Predicate;
+
+public class Radioactive {
+	
+	static final String RADIOACTIVE = "Radioactive";
+	static final String NOT_RADIOACTIVE = "Not Radioactive";
+
+	public static void main(String[] args) {
+		
+		String currentRadioactiveStatus = NOT_RADIOACTIVE;
+		
+		boolean amIRadioactive = testIfRadioactive(currentRadioactiveStatus, new RadioactiveTester());
+		System.out.println("amIRadioactive = " + amIRadioactive);
+		
+		currentRadioactiveStatus = RADIOACTIVE;
+		
+		boolean amILambdaRadioactive = testIfRadioactive(currentRadioactiveStatus, radioactiveStatus -> radioactiveStatus.equals(RADIOACTIVE));
+		System.out.println("amILambdaRadioactive = " + amILambdaRadioactive);
+		
+	}
+	
+	static boolean testIfRadioactive(String radioactiveStatus, Predicate<String> radioactiveTester) {
+		return radioactiveTester.test(radioactiveStatus);
+	}
+
+}
+
+class RadioactiveTester implements Predicate<String> {
+	public boolean test(String radioactiveStatus) {
+		return radioactiveStatus.equals(Radioactive.RADIOACTIVE);
+	}
+}
