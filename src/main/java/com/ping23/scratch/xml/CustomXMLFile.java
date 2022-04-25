45910ec7a515c48bc290618d70f79ed45705c279
James L. Stevenson <jim@ping23.com>
2022-04-25 17:25:35 -0400

    Added xml parsing example (not working, same as the bug I'm working on).

---
+++ src/main/java/com/ping23/scratch/xml/CustomXMLFile.java
@@ -0,0 +1,59 @@
+package com.ping23.scratch.xml;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.xerces.jaxp.DocumentBuilderFactoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class CustomXMLFile {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(CustomXMLFile.class);
+    
+    public static void main(String[] args) {
+        
+        File file = new File("src/main/resources/xml/test1.xml");
+        
+        LOG.info("file: " + file.toString());
+        
+        Document parsedDoc = parseFile(file);
+        
+        LOG.info("parsedDoc: " + parsedDoc.toString());
+        
+    }
+    
+    /** Parses XML file and returns XML document.
+     * @param file String representation of XML file to parse
+     * @return XML document or <B>null</B> if error occured
+     */
+    private static Document parseFile(File file) {
+        DocumentBuilder docBuilder;
+        Document doc = null;
+    
+        try {
+            DocumentBuilderFactory docBuilderFactory = new DocumentBuilderFactoryImpl();
+            docBuilderFactory.setIgnoringElementContentWhitespace(true);
+            docBuilder = docBuilderFactory.newDocumentBuilder();
+
+            doc = docBuilder.parse(file);
+        } catch (ParserConfigurationException e) {
+            LOG.error("Error parsing template file, " + file.getName() + ".", e);
+        } catch (SAXException e) {
+            LOG.error("Error parsing template file, " + file.getName() + ".", e);
+        } catch (IOException e) {
+            LOG.error("Error parsing template file, " + file.getName() + ".", e);
+        }
+
+        return doc;
+    }
+    
+
+
+}
