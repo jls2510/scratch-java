22da6bb562523699bc2bc9011b0f3d9a92c73385
James L. Stevenson <jim@ping23.com>
2022-04-26 08:15:46 -0400

    Workign on xml parsing example

--- src/main/java/com/ping23/scratch/xml/CustomXMLFile.java
+++ src/main/java/com/ping23/scratch/xml/CustomXMLFile.java
@@ -1,5 +1,7 @@
 package com.ping23.scratch.xml;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -8,37 +10,42 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.xerces.jaxp.DocumentBuilderFactoryImpl;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class CustomXMLFile {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(CustomXMLFile.class);
-    
+
     public static void main(String[] args) {
-        
-        File file = new File("src/main/resources/xml/test1.xml");
-        
+
+        File file = new File("src/main/resources/xml/simple0.xml");
+
         LOG.info("file: " + file.toString());
-        
+
         Document parsedDoc = parseFile(file);
-        
+
         LOG.info("parsedDoc: " + parsedDoc.toString());
-        
+
     }
-    
-    /** Parses XML file and returns XML document.
+
+    /**
+     * Parses XML file and returns XML document.
+     * 
      * @param file String representation of XML file to parse
      * @return XML document or <B>null</B> if error occured
      */
     private static Document parseFile(File file) {
         DocumentBuilder docBuilder;
         Document doc = null;
-    
+
         try {
-            DocumentBuilderFactory docBuilderFactory = new DocumentBuilderFactoryImpl();
+            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactoryImpl.newInstance();
             docBuilderFactory.setIgnoringElementContentWhitespace(true);
             docBuilder = docBuilderFactory.newDocumentBuilder();
 
@@ -53,7 +60,21 @@ public class CustomXMLFile {
 
         return doc;
     }
-    
 
+    @Test
+    public void whenGetElementByTag_thenSuccess() {
+        File file = new File("src/main/resources/xml/test1.xml");
 
+        LOG.info("file: " + file.toString());
+
+        Document parsedDoc = parseFile(file);
+
+        NodeList networkNodeList = parsedDoc.getElementsByTagName("network");
+        Node firstNetwork = networkNodeList.item(0);
+
+        assertEquals(1, networkNodeList.getLength());
+        assertEquals(Node.ELEMENT_NODE, firstNetwork.getNodeType());
+        assertEquals("network", firstNetwork.getNodeName());
+        assertEquals("NBC", firstNetwork.getChildNodes().item(0).getNodeValue());
+    }
 }
